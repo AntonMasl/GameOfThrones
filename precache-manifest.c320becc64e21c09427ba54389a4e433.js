d2bc1e69123cc65a2ed2ec0b84227af129205172
Maslov Anton <tosha.maslov.1999@mail.ru>
2020-10-05 14:40:15 +0300

    Updates

---
+++ precache-manifest.c320becc64e21c09427ba54389a4e433.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3d52b22e553db929f53",
+    "url": "/GameOfThrones/static/css/main.8fea915e.chunk.css"
+  },
+  {
+    "revision": "a3d52b22e553db929f53",
+    "url": "/GameOfThrones/static/js/main.a3d52b22.chunk.js"
+  },
+  {
+    "revision": "72a6c7c575d789bf4036",
+    "url": "/GameOfThrones/static/css/1.661c83b7.chunk.css"
+  },
+  {
+    "revision": "72a6c7c575d789bf4036",
+    "url": "/GameOfThrones/static/js/1.72a6c7c5.chunk.js"
+  },
+  {
+    "revision": "ab5973605820d40d8279",
+    "url": "/GameOfThrones/static/js/runtime~main.ab597360.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/GameOfThrones/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "88811ac439c6e0e7059bf09de9b7fc53",
+    "url": "/GameOfThrones/index.html"
+  }
+];
